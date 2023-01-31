b09adb65b28c5b2b8f64607cdb8940f561e41d68
Niraj-Dilshan <nirajnimantha@gmail.com>
2023-01-31 19:20:33 +0530

    foreachchange

--- CountDownTimerDemo/src/com/ictec/threads/CountDownTimerDemo.java
+++ CountDownTimerDemo/src/com/ictec/threads/CountDownTimerDemo.java
@@ -1,20 +1,23 @@
 package com.ictec.threads;
 
+import java.util.ArrayList;
+
 public class CountDownTimerDemo {
     public static void main(String[] args) {
         Runnable flood,ignition,liftoff;
         CountDownTimer ct = new CountDownTimer();
-        flood = new LunchEvent("Flooding...!!!",16);
-        ignition = new LunchEvent("Ignition...!!!",6);
-        liftoff = new LunchEvent("Liftoff...!!!",0);
-        Thread t1 = new Thread(ct);
-        Thread tf = new Thread(flood);
-        Thread ti = new Thread(ignition);
-        Thread tl = new Thread(liftoff);
+        ArrayList<Runnable> events = new ArrayList<Runnable>();
 
+        events.add(new LunchEvent("Flooding...!!!", 16));
+        events.add(new LunchEvent("Ignition...!!!", 6));
+        events.add(new LunchEvent("Liftoff...!!!", 0));
+
+        Thread t1 = new Thread(ct);
         t1.start();
-        tf.start();
-        ti.start();
-        tl.start();
+
+        for(Runnable r: events){
+                new Thread(r).start();
+        }
+
     }
 }
