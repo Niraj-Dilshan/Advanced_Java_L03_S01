4d2a1a9b33d9e8d6e5c0d0efca124f832ce6bd93
Niraj-Dilshan <nirajnimantha@gmail.com>
2023-02-07 12:27:59 +0530

    bug-fix

--- MySerializationDemo/src/com/ictec/serialization/Student.java
+++ MySerializationDemo/src/com/ictec/serialization/Student.java
@@ -4,6 +4,11 @@ public class Student {
     private String name;
     private int id;
 
+    public Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
