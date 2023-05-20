68b6722a65c52da60a4f88841d872ff24ae9065a
INFINITY <nirajnimantha@gmail.com>
2023-05-20 12:46:05 +0530

    init

---
+++ springbootworkshop/src/main/java/com/ictec/springbootworkshop/SpringbootworkshopApplication.java
@@ -0,0 +1,13 @@
+package com.ictec.springbootworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootworkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootworkshopApplication.class, args);
+	}
+
+}
---
+++ springbootworkshop/src/test/java/com/ictec/springbootworkshop/SpringbootworkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ictec.springbootworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootworkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
